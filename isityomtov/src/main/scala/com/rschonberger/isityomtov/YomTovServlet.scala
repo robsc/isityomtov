9f12c0d66d416e19b61b49b8168a7148242df877
Robert Schonberger <rschonberger@gmail.com>
2012-02-20 20:10:33 -0500

    Initial update that implements a date based Friday/Saturday detector.

--- isityomtov/src/main/scala/com/rschonberger/isityomtov/YomTovServlet.scala
+++ isityomtov/src/main/scala/com/rschonberger/isityomtov/YomTovServlet.scala
@@ -1,24 +1,61 @@
 package com.rschonberger.isityomtov
 
+import javax.servlet.http.HttpServletRequest
 import org.scalatra._
+import org.scala_tools.time.Imports._
 import scalate.ScalateSupport
 
 class YomTovServlet extends ScalatraServlet with ScalateSupport {
+  object YomTovStatus extends Enumeration {
+    type YomTovStatus = Value
+    val No, Yes = Value
+    val FromSunset = Value("From sunset on")
+    val UntilSundown = Value("Until sundown")
+  }
+  import YomTovStatus._
 
-  get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+  def isItYomTov(date:org.joda.time.DateTime):YomTovStatus = {
+    (date.dayOfWeek) getAsText match { 
+      case "Friday" => YomTovStatus.FromSunset
+      case "Saturday" => YomTovStatus.UntilSundown
+      case _ => No
+    }
+  }
+
+  get("/teapot") {
+    halt(418, <title>I'm a teapot</title>)
   }
 
   notFound {
-    // Try to render a ScalateTemplate if no route matched
-    findTemplate(requestPath) map { path =>
-      contentType = "text/html"
-      layoutTemplate(path)
-    } orElse serveStaticResource() getOrElse resourceNotFound() 
+    <h1>Not Found.</h1>
+  }
+
+  get("/") { 
+    // Simple javascript: open a GET url with the correct DATE.
+    <html>
+    <head>
+    <script type="text/javascript">
+    date = new Date();
+    window.location = window.location + 'date/' + date.toLocaleDateString();
+    </script>
+    </head>
+    </html>
+  }
+
+  private val formatter: org.joda.time.format.DateTimeFormatter = { 
+    // Strings like "Monday, Feb 20, 2012"
+    org.joda.time.format.DateTimeFormat.forPattern("EEE, MMM dd, YYYY")
+  }
+  
+  get("/date/:date") {
+    val date: String = params("date")
+        val parsed_date: org.joda.time.DateTime = formatter  parseDateTime date
+    <html>
+      <body>
+        <font size='+100'>
+          <center>{isItYomTov(parsed_date)}</center>
+        </font>
+      </body>
+    </html>
   }
 }
